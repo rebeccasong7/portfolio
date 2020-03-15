2956d1a5b3900b5962248da3099b4aadf00a4992
Rebecca Song <rsong@RebeccaSong.local>
2020-03-14 23:22:14 -0400

    Modified directory of homepage

--- src/features/Movie/index.js
+++ src/features/Movie/index.js
@@ -5,8 +5,9 @@ class Movie extends Component {
         console.log(this.props);
       return (       
         <div className="p-grid">
-            <div className="p-col"><h2>{this.props.title}</h2></div>
-            <div className="p-col"><img width="500px" src={this.props.poster} alt=''/>
+            <div className="p-col-3"><h1>Build Test</h1></div>
+            <div className="p-col-4"><h2>{this.props.title}</h2></div>
+            <div className="p-col-4"><img width="100%" src={this.props.poster} alt=''/>
         </div>
 </div>
 
