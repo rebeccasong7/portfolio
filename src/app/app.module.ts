861649cbaab94099e8fba614537d787b084e95da
Rebecca Song <rsong@RebeccaSong.local>
2021-09-16 18:26:08 -0400

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { BodyComponent } from './layout/body/body.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AboutComponent } from './layout/body/about/about.component';
+import { WorkComponent } from './layout/body/work/work.component';
+import { ContactComponent } from './layout/body/contact/contact.component';
+
+const appRoutes: Routes = [
+  { path:'', component: LayoutComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    AboutComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
